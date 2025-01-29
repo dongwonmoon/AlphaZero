1326c0f2664032242722aa4e3ceeb8ddb38d35a3
AI-MoonDongWon <ansehddnjs187@gmail.com>
2025-01-29 09:13:55 +0900

    수정

--- self_play.py
+++ self_play.py
@@ -10,21 +10,17 @@ class SelfPlay:
         self.temperature = temperature
 
     def play(self):
-        mcts1 = MCTS(self.model, self.num_simulations, self.temperature)
-        mcts2 = MCTS(self.model, self.num_simulations, self.temperature)
+        mcts = MCTS(self.model, self.num_simulations, self.temperature)
         states, policies, rewards = [], [], []
-        curr_p = 1
 
         while not self.game.is_game_over():
             state = self.game.get_board_state()
-            mcts = mcts1 if curr_p == 1 else mcts2
             action, policy = mcts.search(self.game)
             states.append(state)
             policies.append(policy)
             self.game = self.game.apply_move(action)
-            curr_p = -curr_p
             print(self.game)
         winner = self.game.get_result(1)
-        rewards = [winner if i % 2 == 0 else -winner for i in range(len(states))]
+        rewards = [winner for i in range(len(states))]
         print(rewards)
         return states, policies, rewards
